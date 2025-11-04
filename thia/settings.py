3233d03f907bd94880ee0293f9eccdd577b8a8fc
spechtacular <tedspecht@gmail.com>
2025-11-03 16:13:56 -0800

    added STATIC_ROOT variable to settings.py

--- thia/settings.py
+++ thia/settings.py
@@ -221,7 +221,8 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATICFILES_DIRS = [ BASE_DIR / 'static', ]
+STATIC_ROOT = BASE_DIR / "staticfiles" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
