20d9ad8c449cbfa9defbc93bc78bff4f28c27e28
spechtacular <tedspecht@gmail.com>
2025-11-10 09:33:13 -0800

    updated staticfiles configuration

--- thia/settings.py
+++ thia/settings.py
@@ -229,7 +229,7 @@ REST_FRAMEWORK = {
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static', ]
-STATIC_ROOT = "/app/staticfiles" 
+STATIC_ROOT = BASE_DIR / "staticfiles" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
